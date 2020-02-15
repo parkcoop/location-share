babff3fd650f1080a8446924bb70a5b61f36f06a
Parker Cooper <parkr.coopr@gmail.com>
2020-02-15 02:23:39 -0500

    get trips for dashboard

--- server/resolvers/Query.js
+++ server/resolvers/Query.js
@@ -1,14 +1,22 @@
 const { User, Trip } = require('./schemas')
 
 
-const users = () => User.find({}, (error, users) => {
-    if (error) throw new Error(error)
-    console.log('users: ', users)
-    return users
-})
+const users = () => User.find({}, 
+    (error, users) => {
+        if (error) throw new Error(error)
+        return users
+    }
+)
 
-// const trips = () => Trip.find()
+const trips = (_, { userId }) => Trip.find({creator: userId}, 
+    (error, trips) => {
+        if (error) throw new Error(error)
+        return trips
+    }
+)
+
 
 module.exports = {
     users,
+    trips
 }
